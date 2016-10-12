503c5d8c9d9f6c7196a13e79df0cff8e4c06d2a0
Simon Doppler <dopsi@dopsi.ch>
2016-10-12 14:20:15 +0200

    Add systemd files

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from distutils.core import setup
 
 setup(name='security-mailer',
     version='1.0',
-    scripts=['scripts/security-mailer']
+    scripts=['scripts/security-mailer'],
+    data_files=[
+        ('lib/systemd/system',
+        ['systemd/security-mailer.timer','systemd/security-mailer.service'])
+    ]
 )
